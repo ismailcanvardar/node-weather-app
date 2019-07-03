34d6f310f579f3b5dc21fe274e130b1191f14a9e
ismailcanvardar@gmail.com <ismailcanvardar@gmail.com>
2019-07-03 18:13:25 +0300

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require('request')
 
+
+
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/79c49fd873350a8f7603501719752cb3/' + latitude + ',' + longitude
 
@@ -9,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. This high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
